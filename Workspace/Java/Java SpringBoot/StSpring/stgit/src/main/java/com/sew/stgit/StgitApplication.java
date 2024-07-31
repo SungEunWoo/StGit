97869f537870f4ced19909875bb2be24fc6da9b3
SungEunWoo <sungeu18@naver.com>
2024-08-01 02:45:42 +0900

    stSpring

---
+++ Workspace/Java/Java SpringBoot/StSpring/stgit/src/main/java/com/sew/stgit/StgitApplication.java
@@ -0,0 +1,13 @@
+package com.sew.stgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StgitApplication.class, args);
+	}
+
+}
---
+++ Workspace/Java/Java SpringBoot/StSpring/stgit/src/test/java/com/sew/stgit/StgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sew.stgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
